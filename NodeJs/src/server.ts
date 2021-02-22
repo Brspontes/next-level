29f61cd9e092855fe8e20980cf13ad9aad771c5d
Brspontes <brian.robert16@hotmail.com>
2021-02-22 12:13:12 -0300

    Dia 1 - configurando ambiente

---
+++ NodeJs/src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express'
+
+const app = express()
+
+app.get("/", (req, res) => {
+  return res.json({
+    message: "Hello Word"
+  })
+})
+
+app.post("/", (req, res) => {
+  return res.json({
+    message: "Dados Salvos"
+  })
+})
+
+app.listen(3333, () => console.log('Server Running'))
