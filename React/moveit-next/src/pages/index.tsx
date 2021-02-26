53413412dbcea67ed08e508200d40d7c3cd325e5
Brspontes <brian.robert16@hotmail.com>
2021-02-26 20:48:58 -0300

    desenvolvendo component de countdown

--- React/moveit-next/src/pages/index.tsx
+++ React/moveit-next/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import { CompletedChellenges } from '../components/CompletedChallenges'
+import { Coutdown } from '../components/Coutdown'
 import { ExperienceBar } from '../components/ExperienceBar'
 import { Profile } from '../components/Profile'
 import  styles  from '../styles/pages/Home.module.css'
@@ -7,14 +8,18 @@ import  styles  from '../styles/pages/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Início | Move.It</title>
+      </Head>
       <ExperienceBar />
       <section>
         <div>
           <Profile />
           <CompletedChellenges />
+          <Coutdown />
         </div>
         <div>
-
+          
         </div>
       </section>
     </div>
